62d47d6d4cf2cf577b7d6c232af2a4987597eb67
Cedric VANGOUT <mail@cvng.dev>
2020-06-14 23:42:19 +0200

    Create mod.ts

---
+++ mod.ts
@@ -0,0 +1,6 @@
+// Copyright 2020 the deno_graphql authors. All rights reserved. MIT license.
+
+export * from "./deps.ts";
+export { graphqlHttp as abcGraphqlHttp } from "./src/abc_graphql.ts";
+export { graphqlHttp as attainGraphqlHttp } from "./src/attain_graphql.ts";
+export { graphqlHttp as oakGraphqlHttp } from "./src/oak_graphql.ts";
