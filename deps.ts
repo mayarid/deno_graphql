1c4e88e1db03b0b89d23e70dd366b67455fee4aa
Ibnu Yahya <anak10thn@gmail.com>
2022-01-23 14:19:27 +0700

    Fix dependency

--- deps.ts
+++ deps.ts
@@ -1,11 +1,16 @@
 // Copyright 2020 the deno_graphql authors. All rights reserved. MIT license.
 
-import gql from "https://cdn.pika.dev/graphql-tag@^2.10.1";
+import gql from "https://cdn.skypack.dev/graphql-tag@^2.10.1";
 
 export { gql };
+import {
+  graphql,
+  GraphQLArgs,
+  ExecutionResult,
+} from "https://cdn.skypack.dev/graphql@^15.0.0";
 export {
   graphql,
   GraphQLArgs,
   ExecutionResult,
-} from "https://cdn.pika.dev/graphql@^15.0.0";
-export { makeExecutableSchema } from "https://cdn.pika.dev/@graphql-tools/schema@^6.0.0";
+}
+export { makeExecutableSchema } from "https://cdn.skypack.dev/@graphql-tools/schema@^6.0.0";
